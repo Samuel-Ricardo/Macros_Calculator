7cd1d7c5c4b53acb2a4eabd9a4b556464759b772
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-04 13:23:30 -0300

    Created: Controller - UserDataActivity (main.java)

---
+++ app/src/main/java/com/study/kotlin/macroscalculator/UserDataActivity.kt
@@ -0,0 +1,12 @@
+package com.study.kotlin.macroscalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserDataActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_data)
+    }
+}
