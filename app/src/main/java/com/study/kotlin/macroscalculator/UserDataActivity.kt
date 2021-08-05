39dac762c9ebcd115d4535ef3c704da884a18549
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-04 23:29:21 -0300

    Refactor: Screen - user_data (layout)

--- app/src/main/java/com/study/kotlin/macroscalculator/UserDataActivity.kt
+++ app/src/main/java/com/study/kotlin/macroscalculator/UserDataActivity.kt
@@ -8,5 +8,7 @@ class UserDataActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_data)
+
+        
     }
 }
